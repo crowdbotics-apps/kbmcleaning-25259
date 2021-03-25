c7d3fb23155c7561a2791cd179c8568dd740cef5
Crowdbotics <team@crowdbotics.com>
2021-03-25 08:46:15 +0000

    Update models

--- backend/kbmcleaning_25259/wsgi.py
+++ backend/kbmcleaning_25259/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kbmcleaning_25259.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kbmcleaning_25259.settings")
 
 application = get_wsgi_application()
